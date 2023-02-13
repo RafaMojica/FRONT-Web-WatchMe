1ec1f0daefa7eb5aaf20bbbf00324794c19be8ef
Rafa Mojica <rafael.mojica27@gmail.com>
2023-02-12 20:55:38 -0500

    FEATURE: Se crea carrusel

--- src/common/Carrusel.jsx
+++ src/common/Carrusel.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function Carrusel() {
+function Carrusel({ posters, type }) {
   return (
-    <div>Carrusel</div>
-  )
+    <div className="container-carrusel">
+      <div className="description-carrusel">
+        <h3>{type} MEJOR PUNTUADAS</h3>
+        <button>Ver mas {type}</button>
+      </div>
+      <div className="poster-carrusel">
+        {posters.map((poster) => (
+          <img src={`https://image.tmdb.org/t/p/w300/${poster?.poster_path}`} alt="poster-img" />
+        ))}
+      </div>
+      <ul className="puntos">
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
 
-export default Carrusel
+export default Carrusel;
