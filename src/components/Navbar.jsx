331045e2b498ce0323f7d35186254164f3c25b68
Rafa Mojica <rafael.mojica27@gmail.com>
2023-02-15 17:58:30 -0500

    FIX: Se cambia link por NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import appearMenu from "../utils/navbar"
 import Logo from "../assets/logoWatchMe.png";
@@ -8,22 +8,22 @@ function Navbar() {
   return (
     <div className="navbar-container">
       <div className="navbar-logo">
-        <Link to="/">
+        <NavLink to="/">
           <img src={Logo} alt="logo" />
-        </Link>
+        </NavLink>
       </div>
 
       <div className="navbar-menu">
         <ul>
-          <li><Link to="/" className="active">Inicio</Link></li>
-          <li><Link to="/movies">Películas</Link></li>
-          <li><Link to="/series">Series</Link></li>
+          <li><NavLink to="/">Inicio</NavLink></li>
+          <li><NavLink to="/movies"> Peliculas</NavLink></li>
+          <li><NavLink to="/series">Series</NavLink></li>
         </ul>
       </div>
 
       <div className="navbar-login">
-        <Link to="/login">Ingresar</Link>
-        <Link to="/register">Regístrarse</Link>
+        <NavLink to="/login">Ingresar</NavLink>
+        <NavLink to="/register">Regístrarse</NavLink>
         <AiOutlineMenu className="icon appear" id="icon-menu" onClick={appearMenu} />
         <AiOutlineClose className="icon" id="icon-close" onClick={appearMenu} />
       </div>
