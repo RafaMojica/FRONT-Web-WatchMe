4f9ffc8663d688929f566043b7c2a7f3993af98f
Rafa Mojica <rafael.mojica27@gmail.com>
2023-02-15 17:57:20 -0500

    FEATURE: Se le agrega common loading

---
+++ src/common/Loading.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div id="contenedor">
+      <div class="contenedor-loader">
+        <div class="loader1"></div>
+        <div class="loader2"></div>
+        <div class="loader3"></div>
+        <div class="loader4"></div>
+      </div>
+      <div class="cargando">Cargando...</div>
+    </div>
+  );
+}
+
+export default Loading;
