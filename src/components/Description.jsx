d883b1e9814d09d267163a8ac2451db5d467efb5
Rafa Mojica <rafael.mojica27@gmail.com>
2023-02-15 17:59:52 -0500

    FEATURE: Agrega funcionalidad al boton de atras

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
-import { selectMovie } from "../states/movies";
-import { selectSerie } from "../states/series";
+import Loading from "../common/Loading";
+import { empySelectMovie, selectMovie } from "../states/movies";
+import { empySelectSerie, selectSerie } from "../states/series";
 
 function Description() {
   const { id, type } = useParams();
@@ -11,26 +12,41 @@ function Description() {
   const serie = useSelector((state) => state.series.select);
   let films = {};
 
-  if(type === "movies") films=movie
-  if(type === "series") films=serie
+  if (type === "movies") films = movie;
+  if (type === "series") films = serie;
 
   useEffect(() => {
-    if (type === "movies") dispatch(selectMovie(id))
-    if(type === "series") dispatch(selectSerie(id))
+    if (type === "movies") dispatch(selectMovie(id));
+    if (type === "series") dispatch(selectSerie(id));
   }, [id, type, dispatch]);
+
+  const buttonBack = () => {
+    window.history.back();
+    dispatch(empySelectMovie());
+    dispatch(empySelectSerie());
+  };
 
   return (
     <>
-      <div key={films?.id} className="img-description" style={{backgroundImage: `url(${process.env.REACT_APP_URL_IMG_ORIGINAL}/${films?.backdrop_path})`}}>
+      <div
+        key={films?.id}
+        className="img-description"
+        style={{
+          backgroundImage: `url(${process.env.REACT_APP_URL_IMG_ORIGINAL}/${films?.backdrop_path})`,
+        }}
+      >
         <div className="description-box">
           <div>
-            <img src={`${process.env.REACT_APP_URL_IMG_W342}/${films?.poster_path}`} alt="img_poster"/>
+            <img
+              src={`${process.env.REACT_APP_URL_IMG_W342}/${films?.poster_path}`}
+              alt="img_poster"
+            />
           </div>
           <div className="description">
             <h2>{films?.title || films?.name}</h2>
             <p>{`Calificación: ${films?.vote_average}`}</p>
             <p>{films?.overview || "Aun no contamos con su descripcion"}</p>
-            <button>Atrás</button>
+            <button onClick={buttonBack}>Atrás</button>
           </div>
         </div>
       </div>
