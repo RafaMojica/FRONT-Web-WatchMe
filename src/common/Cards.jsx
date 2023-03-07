13fdef960619c5b42314791d5c72a373e9f22be8
Rafa Mojica <rafael.mojica27@gmail.com>
2023-03-07 13:09:48 -0500

    FEATURA: Se agraga funcionalidad boton favoritos

--- src/common/Cards.jsx
+++ src/common/Cards.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {AiFillHeart} from "react-icons/ai"
+import { useDispatch } from "react-redux";
+import { addFavorite } from "../states/favorites";
 
-function Cards({ movie }) {
+function Cards({ movie, user }) {
+  const dispatch = useDispatch()
+
   return (
     <div className="card">
       <div className="img-card">
@@ -10,7 +14,7 @@ function Cards({ movie }) {
           <div className="description-card">
             <h5>{movie.title || movie.name}</h5>
             <Link to={`${movie?.id}`}><button>Ver Detalles</button></Link>
-            <button className="favorite-card"><AiFillHeart /></button>
+            <button className="favorite-card" onClick={()=> dispatch(addFavorite({movie, user}))}><AiFillHeart /></button>
           </div>
       </div>
     </div>
