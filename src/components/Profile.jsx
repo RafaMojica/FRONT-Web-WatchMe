5edff9b9ab9df915b2c1d7627017a417fa49c55a
Rafa Mojica <rafael.mojica27@gmail.com>
2023-03-09 20:37:33 -0500

    FEATURE: Se crea vista perfil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "./Navbar";
+import {CgUserlane} from "react-icons/cg";
 
 function Perfil() {
+  const user = useSelector((state) => state.users.user);
+  
   return (
     <div>
       <Navbar />
-      Perfil
+      <div className="perfil-box">
+        <div className="perfil-container">
+          <div className="shape">
+            <div className="perfil-image"><CgUserlane /></div>
+          </div>
+          <h3>Nombre</h3>
+          <h4>{`${user.name} ${user.lastname}`}</h4>
+          <h3>Correo</h3>
+          <h4>{user.email}</h4>
+          <button>Eliminar perfil</button>
+        </div>
+      </div>
     </div>
   );
 }
