df4a2a9884492b083711d713a5d7f61d1ff1b5b8
Rafa Mojica <rafael.mojica27@gmail.com>
2023-03-10 16:01:46 -0500

    FIX: Se arregla carrusel

--- src/common/Carrusel.jsx
+++ src/common/Carrusel.jsx
@@ -1,37 +1,33 @@
 import React from "react";
+import { useSnapCarousel } from "react-snap-carousel";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 function Carrusel({ posters, type, url }) {
+  const { scrollRef, next, prev } = useSnapCarousel();
+
   return (
-    <div className="container-carousel">
-      <div className="name-carousel">
-        <h3>{type} MEJOR PUNTUADAS</h3>
-        <Link to={`${url}`}>
-          <button className="btn-carrousel">Ver mas {type}</button>
-        </Link>
-      </div>
-      <div className="list-carousel" id="list-carousel">
-        <button className="change button-pre">
-          <AiOutlineArrowLeft />
-        </button>
-        <div className="track-carousel" id="track">
-          <div className="carrusel">
+    <>
+      <div className="container-carousel">
+        <div className="description-carrusel">
+          <h3>{type} MEJOR PUNTUADAS</h3>
+          <Link to={`${url}`}>
+            <button className="btn-description">Ver mas {type}</button>
+          </Link>
+        </div>
+        <div className="carrusel">
+          <ul ref={scrollRef} className="list-carousel" >
             {posters.map((poster) => (
               <Link key={poster?.id} to={`${url}/${poster?.id}`}>
-                <img
-                  src={`${process.env.REACT_APP_URL_IMG_W300}/${poster?.poster_path}`}
-                  alt="poster-img"
-                />
+                <img src={`${process.env.REACT_APP_URL_IMG_W300}/${poster?.poster_path}`} alt="poster-img" />
               </Link>
             ))}
-          </div>
+          </ul>
+          <button className="button-pre" onClick={() => prev()}><AiOutlineArrowLeft /></button>
+          <button className="button-next" onClick={() => next()}><AiOutlineArrowRight /></button>
         </div>
-        <button className="change button-next">
-          <AiOutlineArrowRight />
-        </button>
       </div>
-    </div>
+    </>
   );
 }
 
